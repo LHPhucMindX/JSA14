577b83a42908b692d84945f586aedc859424ca52
Phuc Le Hong <hatakraw75@gmail.com>
2021-12-04 15:32:37 +0700

    upgraded phonelist

--- Session4/index4.js
+++ Session4/index4.js
@@ -1,70 +1,157 @@
-let phoneList = [
-    {
-        name: 'Jonh',
-        email: 'john@gmail.com',
-        add:"Long An",
-        mobile: "153-359-4678"
-    },
-    {
-        name: 'Annie',
-        email: 'annie@gmail.com',
-        add: "New York",
-        phone: '86745392075'
-    },
-    {
-        name: 'Rick Atsley',
-        email: 'rickroll@gmail.com',
-        add:"The Void",
-        mobile: "982364535"
-    }
-]
-let table = document.getElementById('table')
-console.log(table);
-
-let listUserRender = phoneList.map((user, index)=>{
-    return `<tr>
-    <td><input type="checkbox" name="check-all"></td>
-    <td>${index + 1}</td>
-    <td>${user.name}</td>
-    <td>${user.phone}</td>
-    <td>${user.email}</td>
-    <td>${user.add}</td>
-    <td>                        
-    <i class="fas fa-pen padding-5" onclick="showFormedit()"></i>
-    <i class="fas fa-trash padding-5" onclick=""></i>
-    </td>
-    </tr>
-`
-
-}).join('\n')
-let formWrapper = document.getElementById('form-wraper')
-let showForm = false
-function showFormedit(){
-  if  (showForm = true){
-      formWrapper.innerHTML =`<form id="edit-info" onsubmit="handleSubmit()">
-  <input type="text" placeholder="name">
-  <input type="number" placeholder="sdt">
-  <input type="email" placeholder="email">
-  <input type="text" placeholder="address">
-  <button type="submit" class="btn bg-aqua">UPDATE</button>
-</form>
-`
-}
-}
-
+let table = document.getElementById("table");
 table.innerHTML = `
-<thead class="row">
-<th ><input type="checkbox" name="check-all"></th>
-<th>Number</th>
+<thead></thead>
+<tr>
+  <th><input type="checkbox"></th>
+  <th>Name</th>
+  <th>Email</th>
+  <th>Address</th>
+  <th>Mobile</th>
+  <th>Action</th>
+</tr>
+</thead>`;
+
+var phoneList;
+if (localStorage.getItem("phoneList")) {
+    phoneList = JSON.parse(localStorage.getItem("phoneList"));
+let renderPhoneList = phoneList.map((value, index) => {
+    return `<tr>
+        <td><input type="checkbox"></td>
+        <td>${value.name}</td>
+        <td>${value.email}</td>
+        <td>${value.address}</td>
+        <td>${value.mobile}</td>
+        <td>
+        <i class="fas fa-pen" onclick=""></i>
+        <i class="fas fa-trash" onclick="deleteName()"></i>
+        </td>
+    </tr>`;
+}).join("\n");
+
+table.innerHTML += `
+    ${renderPhoneList}`;
+} else {
+    phoneList = [];
+}
+let addForm = false;
+function showForm() {
+    addForm = true;
+    if (addForm == true) {
+        document.getElementById("add-change").innerHTML = `
+        <form id="form" onsubmit="return false">
+        <input type="hidden" id="index"/>
+        <input type="text" placeholder="name" name="name" id="name" required/>
+        <input type="email" placeholder="email" name="email" id="email" required/>
+        <input type="text" placeholder="address" required name="address" id="address"/>
+        <input type="text" placeholder="mobile" name="mobile"  id="mobile" required/>
+        <input type="button" class="padding-10 btn bg-aqua" id="save" onclick="addContact(event)" value="Add"/>
+    </form>`;
+    }
+}
+function addContact(event) {
+    event.preventDefault();
+    let name = document.forms["form"]["name"].value;
+    let email = document.forms["form"]["email"].value;
+    let address = document.forms["form"]["address"].value;
+    let mobile = document.forms["form"]["mobile"].value;
+    if (localStorage.getItem("phoneList")) {
+    phoneList = JSON.parse(localStorage.getItem("phoneList"));
+} else {
+    phoneList = [];
+}
+    if (name && email && address && mobile) {
+        phoneList.push({ name, email, address, mobile });
+        localStorage.setItem("phoneList", JSON.stringify(phoneList));
+    }
+let renderPhoneList = phoneList.map((value, index) => {
+    return `<tr>
+        <td><input type="checkbox"></td>
+        <td>${value.name}</td>
+        <td>${value.email}</td>
+        <td>${value.address}</td>
+        <td>${value.mobile}</td>
+        <td>
+        <i class="fas fa-pen" onclick=""></i>
+        <i class="fas fa-trash" onclick="deleteName()"></i>
+    </td>
+    </tr>`;
+}).join("\n");
+  
+table.innerHTML = `
+    <thead></thead>
+    <tr>
+        <th><input type="checkbox"></th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Mobile</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+  ${renderPhoneList}`;
+resetInput();
+}
+function resetInput() {
+    document.forms["form"]["name"].value = "";
+    document.forms["form"]["email"].value = "";
+    document.forms["form"]["address"].value = "";
+    document.forms["form"]["mobile"].value = "";
+}
+function deleteName(key) {
+    if (localStorage.getItem("phoneList")) {
+    phoneList = JSON.parse(localStorage.getItem("phoneList"));
+} else {
+    phoneList = [];
+}
+let i;
+    if (confirm("Are you sure you want to delete this item?")) {
+        phoneList.splice(key, 1);
+        localStorage.setItem("phoneList", JSON.stringify(phoneList));
+}
+let renderPhoneList = phoneList.map((value, index) => {
+    return `<tr>
+        <td><input type="checkbox"></td>
+        <td>${value.name}</td>
+        <td>${value.email}</td>
+        <td>${value.address}</td>
+        <td>${value.mobile}</td>
+        <td>
+        <i class="fas fa-pen" onclick=""></i>
+        <i class="fas fa-trash" onclick="deleteName()"></i>
+        </td>
+    </tr>`;
+}).join("\n");
+  
+table.innerHTML = `
+<thead></thead>
+<tr>
+<th><input type="checkbox"></th>
 <th>Name</th>
-<th>Mobile</th>
 <th>Email</th>
 <th>Address</th>
-<th>Actions</th>
+<th>Mobile</th>
+<th>Action</th>
+</tr>
 </thead>
-${listUserRender}
-`
-
-function handleSubmit(event){
-
+${renderPhoneList}`;
+}
+function deleteAll() {
+    if (localStorage.getItem("phoneList")) {
+        phoneList = JSON.parse(localStorage.getItem("phoneList"));
+} else {
+    phoneList = [];
+}
+    localStorage.clear();
+    console.log(phoneList);
+    table.innerHTML = `
+<thead></thead>
+<tr>
+    <th><input type="checkbox"></th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Address</th>
+    <th>Mobile</th>
+    <th>Action</th>
+</tr>
+</thead>`;
 }
